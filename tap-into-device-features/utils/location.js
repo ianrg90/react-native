3e2c8e95802090ff836075506a48e2112fb6b89c
Ian Gladulich <ian.gladulich@impar.com.br>
2022-05-15 14:54:15 -0300

    Fix

--- tap-into-device-features/utils/location.js
+++ tap-into-device-features/utils/location.js
@@ -1,4 +1,4 @@
-import {GOOGLE_API_KEY} from "@env"
+import {API_KEY} from "@env"
 
 export function getMapPreview (lat, long) {
     const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${long}&zoom=13&size=400x200&maptype=roadmap
