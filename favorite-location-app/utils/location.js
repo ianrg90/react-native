ca1199edb7c5b03e305ebb01c733d3dbb973065e
Ian Gladulich <ian.gladulich@impar.com.br>
2022-05-15 15:04:42 -0300

    Fix

--- favorite-location-app/utils/location.js
+++ favorite-location-app/utils/location.js
@@ -2,7 +2,7 @@ import {API_KEY} from "@env"
 
 export function getMapPreview (lat, long) {
     const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${long}&zoom=13&size=400x200&maptype=roadmap
-    &markers=color:red%7Clabel:S%7C${lat},${long}&key=${GOOGLE_API_KEY}`
+    &markers=color:red%7Clabel:S%7C${lat},${long}&key=${API_KEY}`
     return imagePreviewUrl
 }
 
